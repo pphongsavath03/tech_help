229fd37133f5524e773bccf48453211c2ccc8a9b
pphong <pphongsavath03@gmail.com>
2022-01-06 21:08:10 -0500

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//need to add DB for mongoose connect
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
